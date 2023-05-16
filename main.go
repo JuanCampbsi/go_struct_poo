1347b9c941579ff3356aa3aba64518ab62d75b71
Juan.Ferreira <juan.ferreira@impar.com.br>
2023-05-16 19:12:21 -0300

    :feat: add transfer

--- main.go
+++ main.go
@@ -10,38 +10,58 @@ type AccountCurrent struct {
 }
 
 func main() {
-	accountPhysical := AccountCurrent{title: "Juan", numberAgency: 122, numberAccount: 01545, balance: 125.5}
-	accountPhysical2 := AccountCurrent{title: "Juan", numberAgency: 122, numberAccount: 01545, balance: 125.5}
-	fmt.Println(accountPhysical == accountPhysical2)
-
 	var accountLegal *AccountCurrent
 	accountLegal = new(AccountCurrent)
 	accountLegal.title = "Julia"
-	accountLegal.numberAccount = 999
+	accountLegal.balance = 1000
 
-	accountSlices := []*AccountCurrent{
-		{
-			title:         "Juan",
-			numberAgency:  999,
-			numberAccount: 123456,
-			balance:       1000.0,
-		},
-		{
-			title:         "julia",
-			numberAgency:  888,
-			numberAccount: 654321,
-			balance:       2000.0,
-		},
+	fmt.Println("Before account legal:", accountLegal.balance)
+	fmt.Println(accountLegal.withDrawMoney(200))
+	fmt.Println("After account legal:", accountLegal.balance)
+
+	status, value := accountLegal.depositMoney(400)
+	fmt.Println(status, value)
+
+	var accountPhisical *AccountCurrent
+	accountPhisical = new(AccountCurrent)
+	accountPhisical.title = "Juan"
+	accountPhisical.balance = 1000
+
+	fmt.Println("\nBefore account Phisical:", accountPhisical.balance)
+	fmt.Println(accountPhisical.withDrawMoney(300))
+	fmt.Println("After account Phisical:", accountPhisical.balance)
+
+	statusPhisical, valuePhisical := accountPhisical.depositMoney(-100)
+	fmt.Println(statusPhisical, valuePhisical)
+
+	statusTransfer := accountPhisical.transferAccount(-200, accountLegal)
+	fmt.Println(statusTransfer, *accountPhisical, *accountLegal)
+}
+
+func (c *AccountCurrent) withDrawMoney(value float64) string {
+	if value > 0 && value <= c.balance {
+		c.balance -= value
+		return "Successful withdrawal"
+	} else {
+		return "Insufficient balance"
 	}
-	fmt.Println("before append: ", len(accountSlices))
-	accountSlices = append(accountSlices, &AccountCurrent{
-		title:         "Campos",
-		numberAgency:  999,
-		numberAccount: 123456,
-		balance:       1000.0,
-	})
+}
 
-	fmt.Println("after append: ", len(accountSlices))
-	fmt.Println(*accountSlices[len(accountSlices)-1])
-	fmt.Println(&accountLegal)
+func (c *AccountCurrent) depositMoney(value float64) (string, float64) {
+	if value > 0 {
+		c.balance += value
+		return "\nDeposit made successfully Mr. " + c.title + " your current balance is: ", c.balance
+	} else {
+		return "\nDeposit has not been made Mr. " + c.title + " your current balance is: ", c.balance
+	}
+}
+
+func (c *AccountCurrent) transferAccount(value float64, targetAccount *AccountCurrent) string {
+	if value > 0 && value < c.balance {
+		c.balance -= value
+		targetAccount.depositMoney(value)
+		return "\nMr. " + c.title + " Transfer successfully for " + targetAccount.title
+	} else {
+		return "\nMr. " + c.title + " Transfer unsuccessfuly for " + targetAccount.title
+	}
 }
