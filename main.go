f3fb871a49195d36390dd6a41dd7db3bcf826648
Juan.Ferreira <juan.ferreira@impar.com.br>
2023-05-16 17:03:29 -0300

    :feat: first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type AccountCurrent struct {
+	title         string
+	numberAgency  int
+	numberAccount int
+	balance       float64
+}
+
+func main() {
+	accountPhysical := AccountCurrent{title: "Juan", numberAgency: 122, numberAccount: 01545, balance: 125.5}
+	accountPhysical2 := AccountCurrent{title: "Juan", numberAgency: 122, numberAccount: 01545, balance: 125.5}
+	fmt.Println(accountPhysical == accountPhysical2)
+
+	var accountLegal *AccountCurrent
+	accountLegal = new(AccountCurrent)
+	accountLegal.title = "Julia"
+	accountLegal.numberAccount = 999
+
+	accountSlices := []*AccountCurrent{
+		{
+			title:         "Juan",
+			numberAgency:  999,
+			numberAccount: 123456,
+			balance:       1000.0,
+		},
+		{
+			title:         "julia",
+			numberAgency:  888,
+			numberAccount: 654321,
+			balance:       2000.0,
+		},
+	}
+	fmt.Println("Antes do append: ", len(accountSlices))
+	accountSlices = append(accountSlices, &AccountCurrent{
+		title:         "Campos",
+		numberAgency:  999,
+		numberAccount: 123456,
+		balance:       1000.0,
+	})
+
+	fmt.Println("Depois do append: ", len(accountSlices))
+	fmt.Println(*accountSlices[len(accountSlices)-1])
+	fmt.Println(&accountLegal)
+}
